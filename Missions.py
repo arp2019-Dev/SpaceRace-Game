731a44c6aea2b7888833c428fd4d993413d71b33
arp2019-Dev <130191124+arp2019-Dev@users.noreply.github.com>
2024-08-11 21:22:51 -0700

    Create Missions.py

---
+++ Missions.py
@@ -0,0 +1,12 @@
+Missions = [
+    {"text": "Increase funding for research", "success_chance": 70},
+    {"text": "Send additional cosmonauts for training", "success_chance": 80},
+    {"text": "Collaborate with other Soviet agencies", "success_chance": 60},
+    {"text": "Delay mission to perform additional tests", "success_chance": 75},
+    {"text": "Proceed with mission despite technical concerns", "success_chance": 65},
+    {"text": "Recruit renowned scientist to advise on mission", "success_chance": 85},
+    {"text": "Seek assistance from international partners", "success_chance": 70},
+    {"text": "Conduct secret test flights to gain advantage", "success_chance": 90},
+    {"text": "Increase propaganda efforts to boost morale", "success_chance": 80},
+    {"text": "Implement emergency measures to solve technical issues", "success_chance": 70}
+]
